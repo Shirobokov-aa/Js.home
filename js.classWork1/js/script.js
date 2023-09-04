a5cf566d0c1423e2b4cbca13955fb46a4a4802bc
Shirobokov-aa <136621836+Shirobokov-aa@users.noreply.github.com>
2023-09-04 20:24:20 +0300

    комент

---
+++ js.classWork1/js/script.js
@@ -0,0 +1,109 @@
+
+
+        // ПЕРЕМЕННЫЕ
+// присвоение переменных задается 3 способами
+
+// var name = 0;
+// let name2 = 0;
+// const constName = 0;
+
+// типы данных
+
+// num = 0;  
+// string = "Это строка";
+// bulean = true/false;
+// NaN 
+// null
+// undefined
+// Infinity
+// -Infinity
+
+// тип данных который может содержать любые данные
+// Object
+
+// let x = 20;
+
+// console.log ('test');
+// console.log(x);
+
+// console.log (document);
+// console.log(window);
+
+// операторы
+
+// + 
+// -
+// *
+// /
+// ++  // +1
+// --  // -1
+// **  //возведение в степень
+// %   //остаток от деления
+
+// const block = document.querySelector(".block");
+// console.log(block)
+// const insideBlock = block.querySelector(".insideblock");
+
+
+// alert("какой то тестовый текст");
+
+// confirm("вопрос")
+
+// let value = prompt('введите знчение', '');
+// console.log(value);
+
+// console.log(typeof value)
+
+// console.log (+"1" + + "1");
+
+
+// let value1 = prompt('введите знчение 1', '');
+// let value2 = prompt('введите знчение 2', '');
+// alert ((+value1 + +value2) / 2)
+
+
+
+
+// if (x > y) {
+
+// }
+
+// (x > y)
+// (x < y)
+// (x >= y)
+// (x >= y)
+// (x == y)
+// (x === y)
+// (x != y)
+
+
+// if (2 > 1){
+//     alert('верно')
+// } else {
+//     alert('не верно')
+// }
+
+
+
+// let uls = 0;
+// while(uls < 10){
+//     console.log(uls)
+
+//     uls++;
+// }
+
+// do{
+//     console.log(uls)
+//     uls++;
+// } while (uls <10);
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+
+// for (let i = 0; i < 10; i++){
+//     console.log(i)
+// }
