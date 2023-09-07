0e281e34fea5936d029643c6f2db77cab87ebbe6
Shirobokov-aa <136621836+Shirobokov-aa@users.noreply.github.com>
2023-09-07 18:51:24 +0300

    домашнее изучение js

---
+++ js.home_1/js/alert.js
@@ -0,0 +1,58 @@
+"use strict"
+
+// alert ('Я JavaScript');
+// alert ('Пивет');
+// alert ('Мир');
+
+/* alert ('Hello')
+
+[1, 2].forEach(alert); */
+
+
+
+// let message;
+// message = 'hello';
+
+// alert(message);
+
+// let age;
+// age = 27;
+
+// alert(age);
+
+
+// const myBirthday = '23.09.1995'
+
+// alert(myBirthday);
+
+
+_____________________
+        // Решение задач
+
+
+
+// let lastName;
+// lastName = "Jon";
+// let admin;
+// admin = lastName
+
+// alert(admin);
+
+// let admin, name; // можно объявить две переменные через запятую
+
+// name = "Джон";
+
+// admin = name;
+
+// alert( admin ); // "Джон"
+
+_____________________
+
+// let ourPlanetName = "Земля";
+
+// let currentUserName = "Джон";
+
+______________________
+
+
+
