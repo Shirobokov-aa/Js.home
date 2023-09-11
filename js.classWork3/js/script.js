6632e4f263a4b61f55f54f3639a359f105985167
Shirobokov-aa <136621836+Shirobokov-aa@users.noreply.github.com>
2023-09-11 21:28:57 +0300

    задачи на уроки

---
+++ js.classWork3/js/script.js
@@ -0,0 +1,150 @@
+// let value = prompt("Введит е чесло")
+
+// if (value > 0 ) {
+//     alert("больше ноля")
+
+// } else if (value = 0){ 
+//     value = 0
+//     alert("Ноль");
+
+// } else { 
+//     value < 0
+//     alert("меньше 0")
+// }
+
+
+
+
+// let value = prompt("Укажите ваш возраст от 0 - 120")
+
+// if ((value > 0 ) && (value < 120)) {
+//     alert("Возрст подходящий")
+// } else {
+//     alert("ведите корректный возраст");
+// }
+
+
+
+
+
+// let x = prompt("Укажите x")
+// let y = prompt("Укажите y")
+
+// if (x >= 0 && y >= 0) {
+//     alert("1 четверть")
+// } else if (x <= 0 && y >= 0) {
+//     alert("2 четверть")
+// }else if (x <= 0 && y <= 0) {
+//     alert("3 четверть")
+// }else{
+//     alert("4 четверть")
+// }
+
+
+// let x = prompt("Укажите x")
+// let y = prompt("Укажите y")
+
+// if (x != 0 && y != 0){
+//     if (x >= 0){
+//         if(y >= 0){
+//             alert("1")
+//         }else{
+//             alert("3")
+//         }
+//     }else{
+//         if
+//     }
+// }
+
+
+
+// СВИТЧКЕЙСЫ
+
+// let mon = (+ prompt("Какой месяц"))
+// switch (mon) {
+//     case 1:
+//         alert("Январь")
+//         break;
+//     case 2:
+//          alert("ф")
+//         break;
+//     case 3:
+//         alert("м")
+//         break;
+//     case 4:
+//          alert("а")
+//         break;
+//     case 5:
+//         alert("м")
+//         break;
+//     case 6:
+//          alert("июнь")
+//         break;
+//     case 7:
+//         alert("июль")
+//         break;
+//     case 8:
+//          alert("август")
+//         break;
+//     case 5:
+//         alert("м")
+//         break;
+//     case 6:
+//          alert("июнь")
+//         break;
+//     case 7:
+//         alert("июль")
+//         break;
+//     case 8:
+//          alert("август")
+//         break;
+
+//     default:
+//         alert("такого месяца нет")
+//         break;
+// }
+
+
+//  Учить функции
+
+
+// let x = (+ prompt("введите 1ое число"))
+// let y = (+ prompt("введите 2е число"))
+// let sign = prompt("знак")
+
+// function Calc (xF, yF, signF){
+//     switch (signF) {
+//         case "+":
+//             alert(xF + yF)
+//             break;
+//         case "-":
+//             alert(xF - yF)
+//             break;
+//         case "-":
+//             alert(xF * yF)
+//             break;
+
+//         default:
+//             break;
+//     }
+// }
+
+// Calc(10,5,"+")
+
+
+
+
+
+// ЦИКЛЫ
+
+// let x = +prompt("введи число")
+// for (let index = 0; index < x ; index++) {
+//     alert(x)
+// }
+
+
+let x = +prompt("number")
+for (let index = 0; index >= x; index++) {
+    console.log(x)
+    
+}
