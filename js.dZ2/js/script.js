7c721a4e77ffcd5fb424440175429f73d2a94b93
Shirobokov-aa <136621836+Shirobokov-aa@users.noreply.github.com>
2023-09-17 19:14:50 +0300

    dz2

---
+++ js.dZ2/js/script.js
@@ -0,0 +1,55 @@
+//  Задание 1
+
+// let x = prompt("введите возраст")
+
+// if (x > 0 && x < 3){
+//     alert ("rebenok")
+// }else if(x > 3 && x < 18){
+//     alert ( "podrostok")
+// }else if (x > 18 && x < 60){
+//     alert ("vzrosly")
+// }else{
+//     alert("xz kto")
+// }
+
+
+// Задание 2 
+
+// let x = (+ prompt("vvedy 4eslo"))
+
+// switch (x) {
+//     case 1:
+//         alert("!")
+//         break;
+//     case 2:
+//         alert("@")
+//         break;
+//     case 3:
+//         alert("#")
+//         break;
+//     case 4:
+//         alert("$")
+//         break;
+//     case 5:
+//         alert("%")
+//         break;
+//     case 6:
+//         alert("!")
+//         break;
+//     case 7:
+//         alert("!")
+//         break;
+//     case 8:
+//         alert("!")
+//         break;
+    
+//     default:
+//         alert("4ucJIo He IIoDXoDuT")
+//         break;
+// }
+
+// Задание 3
+
+let x = prompt("Введите трех значное число")
+
+if (x > )
